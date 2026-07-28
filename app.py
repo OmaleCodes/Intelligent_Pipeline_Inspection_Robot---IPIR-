2d3eda4483c61e6a8965afbf8a9cf3c9042905e2
OMALE <omalemoses24@gmail.com>
2026-07-28 02:16:55 +0100

    create and display blank image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ print("OpenCV version:", cv.__version__)
 print("NumPy version:", np.__version__)
 
 # Quick sanity check: create and display a blank image
-#blank = np.zeros((300, 300, 3), dtype=np.uint8)
-#cv.imshow("Sanity Check", blank)
-#cv.waitKey(0)
-#cv.destroyAllWindows()
+blank = np.zeros((300, 300, 3), dtype=np.uint8)
+cv.imshow("Sanity Check", blank)
+cv.waitKey(0)
+cv.destroyAllWindows()
