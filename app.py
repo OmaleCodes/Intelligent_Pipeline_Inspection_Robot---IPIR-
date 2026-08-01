167bf64a7729e6b823f8436d4c5340d8b756a1f3
OMALE <omalemoses24@gmail.com>
2026-08-01 21:23:10 +0100

    implement image processing and defect detection in app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import config
+from vision.preprocess import Pipelinepreprocessor
+from vision.detector import DefectDetector
+
+#Load sample pipe image
+
+frame = cv.imread("images/pipe_cracks.jpg")
+if frame is None:
+    print("Error: Image not found!")
+    exit()
+
+#instance of new preprocessor class
+preprocessor = Pipelinepreprocessor()
+
+#instance of defect dector class
+detector = DefectDetector()
+
+#process the frame through the vision model
+results = preprocessor.process(frame)
+
+#detect cracks and rust
+crack_boxes = detector.detect_cracks(results["clahe_gray"])
+rust_boxes = detector.detect_rust(results["rust_mask"])
+
+
+#draw boxes on the orignal frame
+detector.draw_boxes(frame, crack_boxes, (0, 255, 0), "CRACKS") #use red box for cracks
+detector.draw_boxes(frame, rust_boxes, (0, 165, 255), "RUST") #use orange box for rust
+
+#display the output
+cv.imshow("Raw Input", frame)
+cv.imshow("Modular Preprocessed (Clahe Gray)", results["clahe_gray"])
+cv.imshow("Modular Preprocessed (Rust Mask)", results["rust_mask"])
+cv.imshow("Detected pipeline defects", frame)
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
