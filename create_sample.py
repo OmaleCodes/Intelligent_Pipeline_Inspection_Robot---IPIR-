1f069174f5eee50c86f2dae6a7ce3af2e31ae1a8
OMALE <omalemoses24@gmail.com>
2026-08-11 22:27:21 +0100

    chore: add .gitignore and sample image generator script

---
+++ create_sample.py
@@ -0,0 +1,11 @@
+from database.models import InspectionDatabase
+from dashboard.charts import render_defect_trend
+
+#db = InspectionDatabase()
+#print(db.total_defect())
+
+#data = [('RUN_1786140759', 5), ('RUN_9999999999', 2)]
+#render_defect_trend(data)
+
+
+    
