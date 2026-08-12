a36dd054a1a768ad55ebe196f0ea9ffba4b81c54
OMALE <omalemoses24@gmail.com>
2026-08-12 22:14:54 +0100

    add: implement DISTANCE_ESTIMATOR class for distance calculations

---
+++ vision/distance.py
@@ -0,0 +1,20 @@
+import config
+
+
+class DISTANCE_ESTIMATOR:
+    def __init__(self):
+        self.cumulative_distance_m = 0.0
+        self.mm_per_mps = config.mm_per_pixel
+        self.speed_mps = config.Robot_Speed_mps
+        self.last_time = time.time()
+
+    def pixel_to_mm(self, px_value):
+        px = px_value * self.mm_per_mps
+        return px
+
+    def update_odometry(self):
+        current_time = time.time()
+        dt = current_time - self.last_time        #calculate time passes
+        self.cumulative_distance_m += self.speed_mps * dt         #updates distance  
+        self.last_time = current_time               #updates
+        return self.cumulative_distance_m
